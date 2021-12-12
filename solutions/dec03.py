dfe078f5d86c858566dddf460fdd1b46b88fc32a
David Bender <bender2242@gmail.com>
2021-12-12 10:38:55 -0500

    dec03 part two working

--- solutions/dec03.py
+++ solutions/dec03.py
@@ -1,7 +1,3 @@
-import pandas as pd
-
-df = pd.DataFrame()
-
 with open('/Users/davidbender/Desktop/advent-of-code-2021/inputs/dec03.txt','r') as file:
     lines = file.readlines()
     data = []
@@ -9,8 +5,8 @@ with open('/Users/davidbender/Desktop/advent-of-code-2021/inputs/dec03.txt','r')
         clean_line = line.rstrip('\n')
         data.append(clean_line)
 
-def convert_reading(reading: int, threshold: float = 0.5, gamma:bool = True):
-    if gamma:
+def convert_reading(reading: int, threshold: float = 0.5, most_common:bool = True):
+    if most_common:
         if reading > threshold:
             result = 1
         else:
@@ -22,15 +18,36 @@ def convert_reading(reading: int, threshold: float = 0.5, gamma:bool = True):
             result = 1
     return result
 
-def generate_reading(data_list: list, gamma: bool = True):
+def generate_reading(data: list, gamma: bool = True):
     results = []
     for i in range(12):
         digits = [ int(x[i]) for x in data ]
         reading  = sum(digits) / len(data)
-        result = convert_reading(reading, gamma=gamma)
+        result = convert_reading(reading, most_common=gamma)
         results.append(str(result))
     return int(''.join(results),2)
 
 gamma = generate_reading(data, gamma=True)
 epsilon = generate_reading(data, gamma=False)
-print(gamma * epsilon)
+#print(gamma * epsilon)
+
+def rating_reader(starting_list: list, oxygen: bool = True):
+    data = starting_list
+    results = []
+    for i in range(12):
+        digits = [ int(x[i]) for x in data ]
+        reading  = sum(digits) / len(data)
+        result = convert_reading(reading, most_common=oxygen)
+        print(result)
+        results.append(str(result))
+        
+        new_data = []
+        for entry in data:
+            if entry[i] == result:
+                new_data.append(entry)
+        data = new_data
+    return int(''.join(results),2)
+
+oxygen = rating_reader(data, oxygen=True)
+co2 = rating_reader(data, oxygen=False)
+print(oxygen * co2)
