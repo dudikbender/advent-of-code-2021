c5f50f8ece36632ed2506b782b96a097fdf0823e
David Bender <bender2242@gmail.com>
2021-12-13 11:57:25 -0500

    cleaned up code, no success on part two yet

--- solutions/dec03.py
+++ solutions/dec03.py
@@ -7,12 +7,12 @@ with open('/Users/davidbender/Desktop/advent-of-code-2021/inputs/dec03.txt','r')
 
 def convert_reading(reading: int, threshold: float = 0.5, most_common:bool = True):
     if most_common:
-        if reading > threshold:
+        if reading >= threshold:
             result = 1
         else:
             result = 0
     else:
-        if reading > threshold:
+        if reading >= threshold:
             result = 0
         else:
             result = 1
@@ -31,30 +31,21 @@ gamma = generate_reading(data, gamma=True)
 epsilon = generate_reading(data, gamma=False)
 #print(gamma * epsilon)
 
-def filter_list(list_, range, criteria):
-    for i in range(range):
-        data = []
-        for x in list_:
-            if x[i] == critera:
-                data.append(x)
-    return data
-
 def rating_reader(starting_list: list, oxygen: bool = True):
     data = starting_list
-    results = []
     for i in range(12):
         digits = [ int(x[i]) for x in data ]
         reading  = sum(digits) / len(data)
         result = convert_reading(reading, most_common=oxygen)
-        results.append(str(result))
         new_list = []
         for entry in data:
             if entry[i] == str(result):
                 new_list.append(entry)
         data = new_list
-        if len(new_list) == 0:
+        if len(new_list) == 1:
             break
-    return int(''.join(results),2)
+        
+    return int(''.join(data),2)
 
 oxygen = rating_reader(data, oxygen=True)
 print(oxygen)
